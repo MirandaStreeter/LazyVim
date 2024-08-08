4fc0bcf74b3ac178ed20b2b8e6326293493f9434
Miranda Streeter <miranda@mirandastreeter.com>
2024-08-08 15:56:45 -0700

    My taskbar has a better clock, why would I need another?

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,15 @@
+-- see example.
+
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  opts = function(_, opts)
+    -- replaces LazyVim's default lualine_z
+    -- see https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/ui.lua
+    opts.sections.lualine_z = {
+      function()
+        return os.getenv("USER")
+      end,
+    }
+  end,
+}
