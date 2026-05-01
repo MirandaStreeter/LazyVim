8afd97fbdcf7e904044363dd79d1f59249eb1ef0
MirandaStreeter <miranda@mirandastreeter.com>
2026-05-01 16:44:14 -0700

    Remove clock in the status bar

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,15 @@
+-- Here I'm overriding the status line defaults. See example.lua
+
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  opts = function(_, opts)
+    -- replaces LazyVim's default lualine_z
+    -- see https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/ui.lua
+    opts.sections.lualine_z = {
+      function()
+        return os.getenv("USER")
+      end,
+    }
+  end,
+}
